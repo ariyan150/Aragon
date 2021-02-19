00e04020acc4413ee02e983998290af31c238204
Ariyan <a.abidi150@gmail.com>
2021-02-19 09:24:37 -0800

    complete about us

--- scripts.js
+++ scripts.js
@@ -44,3 +44,28 @@ function Type(t, id, speed) {
 }
 
 
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var imgs = document.querySelectorAll(".card");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+imgs.forEach(img => {
+    img.onclick = function(){
+        modal.style.display = "block";
+        let imgsrc = this.style.backgroundImage.split('(')[1].split(')')[0].split('"')[1]
+        modalImg.src = imgsrc;
+        captionText.innerHTML = this.alt;
+      }
+})
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
