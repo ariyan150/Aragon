632bbd9302e2c4f852b6e0a5a2dd10945046f7d8
Ariyan <a.abidi150@gmail.com>
2021-02-27 13:52:23 -0800

    mohammad 1

--- scripts.js
+++ scripts.js
@@ -68,4 +68,5 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  modalImg.src = '';
 } 
